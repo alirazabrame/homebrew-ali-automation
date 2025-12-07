b18f77d3ce62d112ac9a72a40c55d14834cbfdc8
Ali Raza <araza08@i2cinc.com>
2025-12-07 09:51:51 +0500

    version upgrade

--- Formula/ali-automation.rb
+++ Formula/ali-automation.rb
@@ -1,10 +1,10 @@
 class AliAutomation < Formula
   desc "Automation template for creating Gradle test projects"
   homepage "https://github.com/alirazabrame/homebrew-ali-automation"
-  url "https://github.com/alirazabrame/homebrew-ali-automation/archive/v1.0.3.tar.gz"
+  url "https://github.com/alirazabrame/homebrew-ali-automation/archive/v1.0.4.tar.gz"
   sha256 "8404d35710b2bb981671e66b96b15ffb8661212476c6c66ceaa2b56b9036ab47"
   license "MIT"
-  version "1.0.3"
+  version "1.0.4"
 
   depends_on "gradle" => :optional
   depends_on "openjdk" => :recommended
